ed510f835cc6eb8283cd8c2a522dd738f7a6db40
Juanpiq <juanavarroquintero@gmail.com>
2025-09-26 14:54:53 -0500

    Inicio de proyecto

---
+++ src/main/java/com/MposGlobal/Inventario_JuanNAvarro/InventarioJuanNAvarroApplication.java
@@ -0,0 +1,13 @@
+package com.MposGlobal.Inventario_JuanNAvarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioJuanNAvarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioJuanNAvarroApplication.class, args);
+	}
+
+}
